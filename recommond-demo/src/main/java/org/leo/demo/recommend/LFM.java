f2820ceeabeb1b7f79ab60154aeee4009319eea8
“leobone” <l932@163.com>
2020-07-16 18:17:05 +0800

    upd lfm

--- recommond-demo/src/main/java/org/leo/demo/recommend/LFM.java
+++ recommond-demo/src/main/java/org/leo/demo/recommend/LFM.java
@@ -1,18 +1,22 @@
 package org.leo.demo.recommend;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class LFM {
 
-    private int F = 100;// 隐因子数
     private double α = 0.02;// 梯度下降学习速率，步长
-    private double λ = 0.01;// 正则化参数，防止过拟合
-    private int iterations = 50;// 迭代求解次数
+    private static final int F = 100;// 隐因子数
+    private static final double λ = 0.01;// 正则化参数，防止过拟合
+    private static final int iterations = 50;// 迭代求解次数
 
     private Map<Integer, Double[]> userF = Maps.newHashMap();// 用户-隐因子矩阵
     private Map<Integer, Double[]> itemF = Maps.newHashMap();// 物品-隐因子矩阵
@@ -20,21 +24,11 @@ public class LFM {
     public static void main(String[] args) {
         LFM lfm = new LFM();
         Map<Integer, Map<Integer, Double>> userItemRealData = lfm.buildInitData();
-
-    }
-
-    public void train(Map<Integer, Map<Integer, Double>> userItemRealData) {
-        for (int step = 0; step < this.iterations; step++) {
-            System.out.println("第" + (step + 1) + "次迭代");
-            for (int i = 0; i < 100; i++) {
-                int userId = i + 1;
-                Map<Integer, Double> itemMap = userItemRealData.get(userId);
-                for (Entry<Integer, Double> entry : itemMap.entrySet()) {
-                    int itemId = entry.getKey();
-                    double rating = entry.getValue();
-                }
-            }
-        }
+        lfm.initUserItemFactorMatrix(userItemRealData);
+        lfm.train(userItemRealData);
+        lfm.getRecommondItemByUserId(1, 10, userItemRealData);
+        lfm.getRecommondItemByUserId(2, 10, userItemRealData);
+        lfm.getRecommondItemByUserId(50, 10, userItemRealData);
     }
 
     /**
@@ -54,13 +48,16 @@ public class LFM {
                 Double rating = rand.nextDouble();
 
                 if (userItemRealData.containsKey(userId)) {
+                    if (userItemRealData.get(userId).containsKey(itemId)) {
+                        continue;
+                    }
                     userItemRealData.get(userId).put(itemId, rating);
                 } else {
                     Map<Integer, Double> itemMap = new HashMap<Integer, Double>();
                     itemMap.put(itemId, rating);
                     userItemRealData.put(userId, itemMap);
                 }
-                System.out.println(userId + "-" + itemId + ":" + rating);
+
             }
         }
 
@@ -74,12 +71,12 @@ public class LFM {
      * @param userItemRealData
      */
     public void initUserItemFactorMatrix(Map<Integer, Map<Integer, Double>> userItemRealData) {
-        for (int i = 0; i < 100; i++) {
+        for (Entry<Integer, Map<Integer, Double>> userEntry : userItemRealData.entrySet()) {
             // 随机填充用户-隐因子矩阵
-            int userId = i + 1;
-            Double[] randomUserValue = new Double[this.getF()];
-            for (int j = 0; j < this.getF(); j++) {
-                randomUserValue[j] = Math.random() / Math.sqrt(this.getF());
+            int userId = userEntry.getKey();
+            Double[] randomUserValue = new Double[LFM.F];
+            for (int j = 0; j < LFM.F; j++) {
+                randomUserValue[j] = Math.random() / Math.sqrt(LFM.F);
             }
             this.getUserF().put(userId, randomUserValue);
 
@@ -90,12 +87,42 @@ public class LFM {
                 if (this.getItemF().containsKey(itemId)) {
                     continue;// 物品-隐因子矩阵已存在，不再做处理
                 }
-                Double[] randomItemValue = new Double[this.getF()];
-                for (int j = 0; j < this.getF(); j++) {
-                    randomItemValue[j] = Math.random() / Math.sqrt(this.getF());
+                Double[] randomItemValue = new Double[LFM.F];
+                for (int j = 0; j < LFM.F; j++) {
+                    randomItemValue[j] = Math.random() / Math.sqrt(LFM.F);
                 }
                 this.getItemF().put(itemId, randomItemValue);
             }
+        }
+    }
+
+    public void train(Map<Integer, Map<Integer, Double>> userItemRealData) {
+        for (int step = 0; step < LFM.iterations; step++) {
+            System.out.println("第" + (step + 1) + "次迭代");
+            for (Entry<Integer, Map<Integer, Double>> userEntry : userItemRealData.entrySet()) {
+                int userId = userEntry.getKey();
+                Map<Integer, Double> itemMap = userItemRealData.get(userId);
+                for (Entry<Integer, Double> entry : itemMap.entrySet()) {
+                    int itemId = entry.getKey();// 物品ID
+                    double realRating = entry.getValue();// 真实偏好度
+                    double predictRating = this.predict(userId, itemId);// 预测偏好度
+                    double error = realRating - predictRating;// 偏好度误差
+                    Double[] userVal = this.getUserF().get(userId);
+                    Double[] itemVal = this.getItemF().get(itemId);
+
+                    for (int j = 0; j < LFM.F; j++) {
+                        double uv = userVal[j];
+                        double iv = itemVal[j];
+
+                        uv += this.α * (error * iv - LFM.λ * uv);
+                        iv += this.α * (error * uv - LFM.λ * iv);
+
+                        userVal[j] = uv;
+                        itemVal[j] = iv;
+                    }
+                }
+            }
+            this.α *= 0.9;// 按照随机梯度下降算法的要求，学习速率每步都要进行衰减，目的是使算法尽快收敛
         }
     }
 
@@ -110,44 +137,49 @@ public class LFM {
         double predictRating = 0.0;// 预测评分
         Double[] userValue = this.getUserF().get(userId);
         Double[] itemValue = this.getItemF().get(itemId);
-        for (int i = 0; i < this.getF(); i++) {
+        for (int i = 0; i < LFM.F; i++) {
             predictRating += userValue[i] * itemValue[i];
         }
         return predictRating;
     }
 
-    public int getF() {
-        return F;
+    public List<Map.Entry<Integer, Double>> getRecommondItemByUserId(int userId, int count,
+            Map<Integer, Map<Integer, Double>> userItemRealData) {
+        Map<Integer, Double> result = Maps.newHashMap();
+        Map<Integer, Double> realItemVal = userItemRealData.get(userId);
+        Map<Integer, Double[]> predictItemVal = this.getItemF();
+        // double lowestVal = Double.NEGATIVE_INFINITY;// 最小偏好度，初始为负无穷大
+        for (Integer itemId : predictItemVal.keySet()) {
+            if (realItemVal.containsKey(itemId)) {
+                continue;// 预测偏好度的物品在真实偏好度物品中，不处理
+            }
+            double predictRating = this.predict(userId, itemId);// 预测值
+            if (predictRating < 0) {
+                continue;
+            }
+            result.put(itemId, predictRating);
+        }
+
+        List<Map.Entry<Integer, Double>> list = Lists.newArrayList(result.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Double>>() {
+            // 降序
+            public int compare(Map.Entry<Integer, Double> o1, Map.Entry<Integer, Double> o2) {
+                if (o2.getValue() > o1.getValue()) {
+                    return 1;
+                } else if (o2.getValue() < o1.getValue()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        List<Map.Entry<Integer, Double>> topN = list.subList(0, count);
+        System.out.println("用户" + userId + "前" + count + "个推荐结果：");
+        System.out.println(topN);
+        return topN;
     }
 
-    public void setF(int f) {
-        F = f;
-    }
-
-    public double getΑ() {
-        return α;
-    }
-
-    public void setΑ(double α) {
-        this.α = α;
-    }
-
-    public double getΛ() {
-        return λ;
-    }
-
-    public void setΛ(double λ) {
-        this.λ = λ;
-    }
-
-    public int getIterations() {
-        return iterations;
-    }
-
-    public void setIterations(int iterations) {
-        this.iterations = iterations;
-    }
-
+////////////////////////////////////////////////////////////
     public Map<Integer, Double[]> getUserF() {
         return userF;
     }
